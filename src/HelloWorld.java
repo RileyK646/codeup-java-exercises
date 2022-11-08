23756a039c9c7442f629aaeb2e9409a4d99c7cfc
RileyK646 <RileyKing646@gmail.com>
2022-11-08 12:04:07 -0500

    lets drin java

---
+++ src/HelloWorld.java
@@ -0,0 +1,13 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+
+        int myFavoriteNumber = 64;
+        System.out.println(myFavoriteNumber);
+
+        String myString = "My name is Riley";
+        System.out.println(myString);
+
+    }
+
+}
