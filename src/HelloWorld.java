046bfa9f7abf5eedc9fe1f5ebac9d591a10cce0f
RileyK646 <RileyKing646@gmail.com>
2022-11-08 12:06:36 -0500

    mystring pi

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -5,7 +5,7 @@ public class HelloWorld {
         int myFavoriteNumber = 64;
         System.out.println(myFavoriteNumber);
 
-        String myString = 'C';
+        String myString = 3.14159;
         System.out.println(myString);
 
 
