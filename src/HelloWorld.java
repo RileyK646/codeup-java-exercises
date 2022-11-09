c572a9cff89de73033cfe3e60887712858fab36e
RileyK646 <RileyKing646@gmail.com>
2022-11-09 12:27:21 -0500

    exercise done

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
@@ -10,14 +12,34 @@ public class HelloWorld {
 
         float myNumber = 3.14f;
 
-        int x = 5;
+        /*int x = 5;
         System.out.println(++x);
-        System.out.println(x);
+        System.out.println(x);*/
+/*
 
-        int three = (int) "three";
-        String theNumberThree = "three";
-        Object o = theNumberThree;
-        int three = (int) o;
+        int x = 4;
+        System.out.println(x += 5);
+*/
+
+       /* int x = 3;
+        int y = 4;
+        System.out.println(x += y);
+*/
+        int x = 10;
+        int y = 2;
+        System.out.println(x /= y);
+        System.out.println( y -x);
+
+        //ToDo: save your age to a variable. create a scanner class. have the user guess your age. return whether or not the guessed age is the same as the age saved:
+       /* int age = 32;
+        Scanner ageAsl = new Scanner(System.in);
+
+        System.out.print("Whats your age");
+        int userAge = ageAsl.nextInt();
+
+
+        System.out.println(userAge == age);
+*/
     }
 
 }
