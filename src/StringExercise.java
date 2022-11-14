3ec1051ec3d503c54dc7e5b968f31e94aa565f5e
RileyK646 <RileyKing646@gmail.com>
2022-11-14 12:17:06 -0500

    first string

---
+++ src/StringExercise.java
@@ -0,0 +1,7 @@
+public class StringExercise {
+    public static void  main(String[] args) {
+        String message = " We don't need no education";
+        message = message + "\n" + "We dont need no thought control";
+        System.out.println(message);
+    }
+}
